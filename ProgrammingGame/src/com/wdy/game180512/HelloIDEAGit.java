6147413ec6a2147204f4f40e59b934ab663824d0
Wiabw <wangdeyongvip@126.com>
2018-05-12 15:03:28 +0800

    测试 IDEA 的 Git

--- ProgrammingGame/src/com/wdy/game180512/HelloIDEAGit.java
+++ ProgrammingGame/src/com/wdy/game180512/HelloIDEAGit.java
@@ -3,5 +3,6 @@ package com.wdy.game180512;
 public class HelloIDEAGit {
     public static void  main(String[] args) {
         System.out.println("HelloIDEAGit");
+        System.out.println("HelloIDEAGita");
     }
 }
